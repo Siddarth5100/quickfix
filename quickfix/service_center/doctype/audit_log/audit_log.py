c107d3d1ad3bdcdaebecf6b904556e900e00abcf
Siddarth5100 <siddarthsiddhu5100@gmail.com>
2026-03-08 16:32:22 +0530

    feat: add audit log entry for all doctypes using doc_event wildcard

--- quickfix/service_center/doctype/audit_log/audit_log.py
+++ quickfix/service_center/doctype/audit_log/audit_log.py
@@ -1,9 +1,24 @@
 # Copyright (c) 2026, Siddarth and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe.utils import now_datetime
 
 
 class AuditLog(Document):
 	pass
+
+# Add a wildcard doc_events entry that logs every save of every DocType
+def log_change(doc, method):	
+	if doc.doctype == "Audit Log":
+		return
+
+	frappe.get_doc({
+		"doctype": 'Audit Log',
+		"doctype_name": doc.doctype,
+		"document_name": doc.name,
+		"action": method,
+		"user": frappe.session.user,
+		"timestamp": now_datetime()
+	}).insert()
